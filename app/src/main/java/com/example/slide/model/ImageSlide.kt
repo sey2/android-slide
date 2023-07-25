2ee0cf043b8f44d933016005a88b5c39b6ba6d6d
casio2978 <casio2978@naver.com>
2023-07-25 18:10:21 +0900

    [Fix] 코드 리뷰 내용 반영 (07.24)

--- app/src/main/java/com/example/slide/model/ImageSlide.kt
+++ app/src/main/java/com/example/slide/model/ImageSlide.kt
@@ -1,15 +1,10 @@
 package com.example.slide.model
-
-import android.os.Build
-import androidx.annotation.RequiresApi
-
 class ImageSlide(
     override val id: String,
     override var sideLength: Int,
     override var alpha: Int,
     var imageBytes: ByteArray? = byteArrayOf()
 ) : Slide {
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun toString(): String =
         "($id), Slide:$sideLength, Alpha: $alpha"
 }
--- app/src/main/java/com/example/slide/ui/SlideView.kt
+++ app/src/main/java/com/example/slide/ui/SlideView.kt
@@ -1,4 +1,4 @@
-package com.example.slide.view
+package com.example.slide.ui
 
 import android.annotation.SuppressLint
 import android.content.Context
