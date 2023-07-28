3b50db46ae7b4af63fbd04a4fbf0691997c2cd9b
casio2978 <casio2978@naver.com>
2023-07-28 11:23:33 +0900

    [feat] Util 기능 수정 (07.27)

--- app/src/main/java/com/example/slide/factory/SlideCreationFactory.kt
+++ app/src/main/java/com/example/slide/factory/SlideCreationFactory.kt
@@ -4,9 +4,15 @@ import DrawingSlide
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
+import android.util.Base64
+import com.example.slide.db.SlideEntity
+import com.example.slide.drawing.PathData
+import com.example.slide.drawing.PathOperation
+import com.example.slide.drawing.PathOperationAdapter
 import com.example.slide.model.ImageSlide
 import com.example.slide.model.Slide
 import com.example.slide.model.SquareSlide
+import com.google.gson.GsonBuilder
 import java.util.UUID
 import kotlin.random.Random
 
@@ -33,13 +39,14 @@ object SlideCreationFactory {
         var id = createUUID()
 
         val path = Path()
+        val randomColor = Color.argb(255, Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
         val paint = Paint().apply {
-            color = Color.argb(255, Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+            color = randomColor
             style = Paint.Style.STROKE
             strokeWidth = 5f
         }
 
-        return DrawingSlide(id, 0, sideLength, path, paint)
+        return DrawingSlide(id, 0, sideLength, randomColor, path, paint)
     }
 
 
@@ -57,4 +64,69 @@ object SlideCreationFactory {
 
         return id
     }
+
+    fun createSlideFromEntity(slideEntity: SlideEntity): Slide {
+        return when (slideEntity.type) {
+            "ImageSlide" -> createImageSlideFromEntity(slideEntity)
+            "DrawingSlide" -> createDrawingSlideFromEntity(slideEntity)
+            "SquareSlide" -> createSquareSlideFromEntity(slideEntity)
+            else -> throw IllegalArgumentException("Unknown slide type: ${slideEntity.type}")
+        }
+    }
+
+    private fun createImageSlideFromEntity(slideEntity: SlideEntity): ImageSlide {
+        return ImageSlide(
+            id = slideEntity.id,
+            sideLength = slideEntity.sideLength,
+            alpha = slideEntity.alpha,
+            imageBytes = slideEntity.imageBytes?.let {
+                Base64.decode(
+                    slideEntity.imageBytes,
+                    Base64.DEFAULT
+                )
+            },
+            lastPosition = Pair(slideEntity.lastX, slideEntity.lastY)
+        )
+    }
+
+    private fun createDrawingSlideFromEntity(slideEntity: SlideEntity): DrawingSlide {
+        val gson = GsonBuilder()
+            .registerTypeAdapter(PathOperation::class.java, PathOperationAdapter())
+            .create()
+        val pathData = gson.fromJson(slideEntity.pathData, PathData::class.java)
+        val path = Path()
+        for (operation in pathData.operations) {
+            when (operation) {
+                is PathOperation.MoveTo -> path.moveTo(operation.x, operation.y)
+                is PathOperation.LineTo -> path.lineTo(operation.x, operation.y)
+                is PathOperation.QuadTo -> path.quadTo(operation.lastX, operation.lastY, operation.endX, operation.endY)
+            }
+        }
+        return DrawingSlide(
+            id = slideEntity.id,
+            sideLength = slideEntity.sideLength,
+            backgroundColor = slideEntity.backgroundColor,
+            alpha = slideEntity.alpha,
+            paint = Paint(),
+            isEditable = false,
+            path = path,
+            minX = slideEntity.minX,
+            minY = slideEntity.minY,
+            maxX = slideEntity.maxX,
+            maxY = slideEntity.maxY,
+            pathData = pathData,
+            lastPosition = Pair(slideEntity.lastX, slideEntity.lastY)
+        )
+    }
+
+    private fun createSquareSlideFromEntity(slideEntity: SlideEntity): SquareSlide {
+        return SquareSlide(
+            id = slideEntity.id,
+            sideLength = slideEntity.sideLength,
+            alpha = slideEntity.alpha,
+            backgroundColor = slideEntity.backgroundColor ?: 0,
+            lastPosition = Pair(slideEntity.lastX, slideEntity.lastY)
+        )
+    }
+
 }
