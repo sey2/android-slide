6a37779674a5bb63ba4ca9d8a8026afebc8f5441
casio2978 <casio2978@naver.com>
2023-07-28 11:18:24 +0900

    [feat] 새문서 작성하기 기능 추가(07.26)

--- app/src/main/java/com/example/slide/action/SlideAction.kt
+++ app/src/main/java/com/example/slide/action/SlideAction.kt
@@ -5,11 +5,8 @@ import com.example.slide.model.Slide
 sealed class SlideAction {
     data class SelectSlide(val id: String) : SlideAction()
     data class ChangeImage(val slide: Slide, val newImageBytes: ByteArray) : SlideAction()
-    data class ChangeColor(val color: Int) : SlideAction()
-    data class ChangeAlpha(val alpha: Int) : SlideAction()
     data class SetSlides(val slides: List<Slide>) : SlideAction()
     object ChangeSelectedIndex: SlideAction()
-    object AddSlide : SlideAction()
     object ClearSelectedSlide : SlideAction()
-    object AddSlideFromServer : SlideAction()
+    object ClearAllSlide : SlideAction()
 }
